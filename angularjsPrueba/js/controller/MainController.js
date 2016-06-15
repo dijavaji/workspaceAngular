c35e93ec7ad7131e763cfa40a74abcee592a8782
Diego Vásquez <jvasquez@kruger.com.ec>
2016-06-15 02:03:41 -0500

    primer commit

---
+++ angularjsPrueba/js/controller/MainController.js
@@ -0,0 +1,24 @@
+app.controller('MainController',['$scope',function($scope){
+	$scope.titulo = 'technoloqie top seller';
+	$scope.promocion = 'cualquier valor';
+	$scope.producto = {
+		nombre:'Nombre imagen',
+		precio: 19,
+		pubfecha: new Date('2014', '03', '08')
+	};
+	$scope.productos = [
+	{
+	 	name: 'The Book of Trees', 
+	    price: 19, 
+	    pubdate: new Date('2014', '03', '08'), 
+	    cover: 'img/the-book-of-trees.jpg'  
+    },
+     { 
+	    name: 'Program or be Programmed', 
+	    price: 8, 
+	    pubdate: new Date('2013', '08', '01'), 
+	    cover: 'img/program-or-be-programmed.jpg' 
+  	} 
+	];
+
+}])
