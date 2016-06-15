e066910c27016b0b948a1e0498a3a2656d016668
Diego Vásquez <jvasquez@kruger.com.ec>
2016-06-15 12:46:42 -0500

    finalizacion de la unidad 1 codecacademy

--- angularjsPrueba/js/controller/MainController.js
+++ angularjsPrueba/js/controller/MainController.js
@@ -11,14 +11,19 @@ app.controller('MainController',['$scope',function($scope){
 	 	name: 'The Book of Trees', 
 	    price: 19, 
 	    pubdate: new Date('2014', '03', '08'), 
-	    cover: 'img/the-book-of-trees.jpg'  
+	    cover: 'img/images.png',
+	    likes : 0  
     },
      { 
 	    name: 'Program or be Programmed', 
 	    price: 8, 
 	    pubdate: new Date('2013', '08', '01'), 
-	    cover: 'img/program-or-be-programmed.jpg' 
+	    cover: 'img/logo.jpg' ,
+	    likes : 0
   	} 
 	];
+	$scope.masUno = function(index) { 
+  	$scope.productos[index].likes += 1; 
+	};
 
 }])
