bbb8058b023bcc8beba67bff56829ba031145ce1
djvasquezj <djvasquezj@uce.edu.ec>
2018-10-23 14:19:06 -0500

    agrego maincontroller

--- angularjsPrueba/js/app.js
+++ angularjsPrueba/js/app.js
@@ -1,1 +1,7 @@
-var app = angular.module('miApp', []);
+/*Aqui escribimos piezas de nuestra aplicacion Angular. 
+Encapsulamos codigo en los modulos, entonces esto hace que nuestro codigo sea 
+mantenible, mas testeable y que tenga una mejor lectura.
+En un modulo tambien definimos las dependencias de nuestra aplicacion, 
+por ejemplo un modulo puede depender de otros 2 modulos. 
+*/ 
+var app = angular.module('miApp', []);	// Declaramos un modulo y el nombre de la aplicación "miApp"
