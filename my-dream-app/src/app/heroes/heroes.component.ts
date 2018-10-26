73aaa35b87aec7393a398ff270dde7e6de353470
djvasquezj <djvasquezj@uce.edu.ec>
2018-10-26 17:12:59 -0500

    agrego propiedad en componente de heroes y agrego a componente principal

--- my-dream-app/src/app/heroes/heroes.component.ts
+++ my-dream-app/src/app/heroes/heroes.component.ts
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+	nombreHeroe = 'Spiderman';
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
