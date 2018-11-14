2db3661711b131861d2b94d3cc0f7afee0615878
djvasquezj <djvasquezj@uce.edu.ec>
2018-11-13 22:44:34 -0500

    correccion de objeto heroe

--- my-dream-app/src/app/clases/Heroe.ts
+++ my-dream-app/src/app/clases/Heroe.ts
@@ -1,7 +1,7 @@
 export class Heroe {
   id: number;
   nombre: string;
-  apellido string;
-  nacionalidad string;
-  poder string;
+  apellido: string;
+  nacionalidad: string;
+  poder: string;
 }
