18cc7c2a00faa341b195a634d395701206a39fb6
djvasquezj <djvasquezj@uce.edu.ec>
2018-11-21 03:13:50 -0500

    agrego todas propiedades

--- my-dream-app/src/app/controlador/mock-heroes.ts
+++ my-dream-app/src/app/controlador/mock-heroes.ts
@@ -1,14 +1,14 @@
 import {Heroe} from '../clases/Heroe';
 
 export const HEROES: Heroe[] = [
-  { id: 11, nombre: 'Mr. Nice' },
-  { id: 12, nombre: 'Narco' },
-  { id: 13, nombre: 'Bombasto' },
-  { id: 14, nombre: 'Celeritas' },
-  { id: 15, nombre: 'Magneta' },
-  { id: 16, nombre: 'RubberMan' },
-  { id: 17, nombre: 'Dynama' },
-  { id: 18, nombre: 'Dr IQ' },
-  { id: 19, nombre: 'Magma' },
-  { id: 20, nombre: 'Tornado' }
+  { id: 11, nombre: 'Mr. Nice', apellido: 'Parker', nacionalidad: 'neoyorquino', poder: null},
+  { id: 12, nombre: 'Narco' apellido: 'Turner', nacionalidad: 'alburquense', poder: null},
+  { id: 13, nombre: 'Bombasto' apellido: 'bombazo', nacionalidad: 'ecuatoriano', poder: null},
+  { id: 14, nombre: 'Celeritas' apellido: 'Gonzales', nacionalidad: 'ecuatoriano', poder: null},
+  { id: 15, nombre: 'Magneta' apellido: 'Rosero', nacionalidad: 'ecuatoriano', poder: null},
+  { id: 16, nombre: 'RubberMan' apellido: 'Baquero', nacionalidad: 'español', poder: null},
+  { id: 17, nombre: 'Dynama' apellido: 'Cruz', nacionalidad: 'ecuatoriano', poder: null},
+  { id: 18, nombre: 'Dr IQ' apellido: 'Moreno', nacionalidad: 'ecuatoriano', poder: null},
+  { id: 19, nombre: 'Magma' apellido: 'Pichincha', nacionalidad: 'ecuatoriano', poder: null},
+  { id: 20, nombre: 'Tornado' apellido: 'Vargas', nacionalidad: 'ecuatoriano', poder: null}
 ];
