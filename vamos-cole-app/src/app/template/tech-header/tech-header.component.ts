96c1cbb9668a1529d8fbf895da47a28d2930e532
diego-mac <dijavaji@gmail.com>
2019-06-26 20:33:30 -0500

    agregando templates

---
+++ vamos-cole-app/src/app/template/tech-header/tech-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tech-header',
+  templateUrl: './tech-header.component.html',
+  styles: []
+})
+export class TechHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
