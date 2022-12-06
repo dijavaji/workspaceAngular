717479d659d0c2e7bd7374052316a8228bcbc0c8
diego-mac <dijavaji@gmail.com>
2022-12-06 10:22:26 -0500

    agrego sweetalert2 para mensajes personalizados

--- clientes-app/src/app/pages/clientes/cliente-form/cliente-form.component.ts
+++ clientes-app/src/app/pages/clientes/cliente-form/cliente-form.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router} from '@angular/router';
+import swal from 'sweetalert2'
 import { Cliente } from '../../../modelo/cliente';
 import { ClienteService } from '../../../services/cliente.service';
+
 
 @Component({
   selector: 'app-cliente-form',
@@ -22,9 +24,11 @@ export class ClienteFormComponent implements OnInit {
 	public crear(): void{
 		console.log("crear click");
 		console.log(this.cliente);
-		this.clienteService.creaCliente(this.cliente).subscribe(
-			response => this.router.navigate(['/clientes'])
-			);
+		this.clienteService.creaCliente(this.cliente).subscribe(cliente => {
+        this.router.navigate(['/clientes']);
+        swal('Nuevo cliente', `Cliente ${cliente.nombre} creado con éxito!`, 'success');
+      }
+      );
 
 	}
 
